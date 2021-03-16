db97d82bce993ab2e1efb29a5d41cae5126f76a2
zhangxin <13622872688@163.com>
2021-03-16 11:30:11 +0800

    not use requestAnimationFrame

--- editor/inspector/assets/render-flow.js
+++ editor/inspector/assets/render-flow.js
@@ -21,29 +21,27 @@ exports.update = function (assetList, metaList) {
     this.meta = this.metas[0];
     this.assetInfos = assetList;
     this.assetInfo = assetList[0];
-    requestAnimationFrame(() => {
-        this.renderFlow = await Editor.Message.request('scene', 'query-render-flow', this.assetInfo.uuid);
-        const children = this.$.content.childNodes;
+    this.renderFlow = await Editor.Message.request('scene', 'query-render-flow', this.assetInfo.uuid);
+    const children = this.$.content.childNodes;
 
-        let i = 0;
-        for (const key in this.renderFlow) {
-            const dump = this.renderFlow[key];
-            if (!dump.visible) {
-                continue;
-            }
-            let node = children[i];
-            if (!node) {
-                node = document.createElement('ui-prop');
-                this.$.content.appendChild(node);
-                node.addEventListener('change-dump', this.onDataChanged.bind(this));
-                node.setAttribute('type', 'dump');
-            }
-            node.render(dump);
-            i++;
+    let i = 0;
+    for (const key in this.renderFlow) {
+        const dump = this.renderFlow[key];
+        if (!dump.visible) {
+            continue;
         }
-        for (let index = children.length - 1; index > i - 1; index--) {
-            const element = children[index];
-            element.remove();
+        let node = children[i];
+        if (!node) {
+            node = document.createElement('ui-prop');
+            this.$.content.appendChild(node);
+            node.addEventListener('change-dump', this.onDataChanged.bind(this));
+            node.setAttribute('type', 'dump');
         }
-    });
+        node.render(dump);
+        i++;
+    }
+    for (let index = children.length - 1; index > i - 1; index--) {
+        const element = children[index];
+        element.remove();
+    }
 };
