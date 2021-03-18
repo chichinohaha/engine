48ac6b832b1dbe55b9aca4cec53248929d623da1
zhangxin <13622872688@163.com>
2021-03-18 16:12:00 +0800

    add skeleton inspector

--- editor/inspector/assets.js
+++ editor/inspector/assets.js
@@ -18,4 +18,5 @@ module.exports = {
     'audio-clip': join(__dirname, './assets/audio-clip.js'),
     javascript: join(__dirname, './assets/javascript.js'),
     typescript: join(__dirname, './assets/typescript.js'),
+    skeleton: join(__dirname, './assets/skeleton.js'),
 };
