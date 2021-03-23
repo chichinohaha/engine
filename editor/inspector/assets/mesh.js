dfd91f75792c96d23c7d418c36ab1443ab98f032
zhangxin <13622872688@163.com>
2021-03-23 16:08:49 +0800

    Update mesh.js

--- editor/inspector/assets/mesh.js
+++ editor/inspector/assets/mesh.js
@@ -1,6 +1,6 @@
-const ElectronModule = require('@base/electron-module');
-
-const GlPreview = ElectronModule.require('PreviewExtends').default;
+/* eslint-disable @typescript-eslint/no-this-alias */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+const GlPreview = Editor._Module.require('PreviewExtends').default;
 const glPreview = new GlPreview('scene:mesh-preview', 'query-mesh-preview-data');
 
 exports.style = `
@@ -64,10 +64,6 @@ exports.template = `
 `;
 
 exports.methods = {
-    async refresh () {
-        this.isPreviewDataDirty = true;
-        await this.initPreview();
-    },
 
     async initPreview () {
         if (!this.$.image) {
@@ -79,7 +75,7 @@ exports.methods = {
         }
 
         await glPreview.init({ width: this.$.image.clientWidth, height: this.$.image.clientHeight });
-        await Editor.Message.request('scene', 'set-mesh-preview-mesh', this.info.uuid);
+        await Editor.Message.request('scene', 'set-mesh-preview-mesh', this.assetInfo.uuid);
 
         // 增加容错：await 后 inspector 内容变动，会导致 .width 取值报错
         if (!this.$.image) {
@@ -89,67 +85,49 @@ exports.methods = {
         glPreview.initGL(this.$.image, { width: this.$.image.clientWidth, height: this.$.image.clientHeight });
         this.previewConfig.width = this.$.image.clientWidth;
         this.previewConfig.height = this.$.image.clientHeight;
-        if (this.updateTask) {
-            cancelAnimationFrame(this.updateTask);
-            this.updateTask = null;
-        }
 
         await this.update();
     },
 
     async update () {
         const canvas = this.$.image;
-        if (!this || !canvas) {
-            this.updateTask = null;
-            return;
+        // 必须要设置 canvas的宽高
+        canvas.width = this.$.imageDiv.clientWidth;
+        canvas.height = this.$.imageDiv.clientHeight;
+
+        const info = await glPreview.queryPreviewData({ width: this.$.imageDiv.clientWidth, height: this.$.imageDiv.clientHeight });
+        console.log(info);
+        if (info.width !== this.previewConfig.width || info.height !== this.previewConfig.height) {
+            glPreview.resizeGL(info.width, info.height);
+            this.previewConfig.width = info.width;
+            this.previewConfig.height = info.height;
         }
-
-        if (this.isPreviewDataDirty) {
-            // 必须要设置 canvas的宽高
-            canvas.width = this.$.imageDiv.clientWidth;
-            canvas.height = this.$.imageDiv.clientHeight;
-
-            const info = await glPreview.queryPreviewData({ width: this.$.imageDiv.clientWidth, height: this.$.imageDiv.clientHeight });
-
-            if (info.width !== this.previewConfig.width || info.height !== this.previewConfig.height) {
-                glPreview.resizeGL(info.width, info.height);
-                this.previewConfig.width = info.width;
-                this.previewConfig.height = info.height;
-            }
-            try {
-                glPreview.drawGL(info.buffer, info.width, info.height);
-            } catch (e) {
-                console.warn(e);
-            }
-            this.isPreviewDataDirty = false;
+        try {
+            glPreview.drawGL(info.buffer, info.width, info.height);
+        } catch (e) {
+            console.warn(e);
         }
-
-        // eslint-disable-next-line no-void
-        this.updateTask = requestAnimationFrame(() => { void this.update(); });
     },
 
     onPreviewMouseDown (event) {
         Editor.Message.request('scene', 'on-mesh-preview-mouse-down', { x: event.x, y: event.y });
         event.target.requestPointerLock();
-        document.addEventListener('mousemove', this.onPreviewMouseMove);
-        document.addEventListener('mouseup', this.onPreviewMouseUp);
+        const panel = this;
+        async function onPreviewMouseMove (event) {
+            console.log('mousexy', event.movementX, event.movementY);
+            await Editor.Message.request('scene', 'on-mesh-preview-mouse-move', { movementX: event.movementX, movementY: event.movementY });
+            await panel.update();
+        }
 
-        this.isPreviewDataDirty = true;
-    },
-
-    onPreviewMouseMove (event) {
-        Editor.Message.request('scene', 'on-mesh-preview-mouse-move', { movementX: event.movementX, movementY: event.movementY });
-
-        this.isPreviewDataDirty = true;
-    },
-
-    onPreviewMouseUp (event) {
-        Editor.Message.request('scene', 'on-mesh-preview-mouse-up', { x: event.x, y: event.y });
-        document.exitPointerLock();
-        document.removeEventListener('mousemove', this.onPreviewMouseMove);
-        document.removeEventListener('mouseup', this.onPreviewMouseUp);
-
-        this.isPreviewDataDirty = true;
+        async function onPreviewMouseUp (event) {
+            await Editor.Message.request('scene', 'on-mesh-preview-mouse-up', { x: event.x, y: event.y });
+            document.exitPointerLock();
+            document.removeEventListener('mousemove', onPreviewMouseMove);
+            document.removeEventListener('mouseup', onPreviewMouseUp);
+            await panel.update();
+        }
+        document.addEventListener('mousemove', onPreviewMouseMove);
+        document.addEventListener('mouseup', onPreviewMouseUp);
     },
 
     updateMeshInfo (meshInfo) {
@@ -177,18 +155,19 @@ exports.methods = {
 };
 
 exports.ready = function () {
-    Editor.Message.addBroadcastListener('scene:mesh-preview-mesh-info', this.updateMeshInfo);
+    this.updateMeshInfoFunc = this.updateMeshInfo.bind(this);
+    Editor.Message.addBroadcastListener('scene:mesh-preview-mesh-info', this.updateMeshInfoFunc);
     this.previewConfig = {};
-    this.$.image.addEventListener('mousedown', this.onPreviewMouseDown);
+    this.$.image.addEventListener('mousedown', this.onPreviewMouseDown.bind(this));
 };
 
-exports.update = function (assetList, metaList) {
+exports.update = async function (assetList, metaList) {
     this.assetInfo = assetList[0];
     this.assetInfos = assetList;
     this.meta = metaList[0];
     this.metas = metaList;
     this.readonly = this.assetInfo.readonly;
-    this.refresh();
+    await this.initPreview();
 };
 
 exports.data = function () {
@@ -201,7 +180,7 @@ exports.data = function () {
     };
 };
 
-exports.destroyed = function () {
-    Editor.Message.removeBroadcastListener('scene:mesh-preview-mesh-info', this.updateMeshInfo);
+exports.close = function () {
+    Editor.Message.removeBroadcastListener('scene:mesh-preview-mesh-info', this.updateMeshInfoFunc);
     Editor.Message.request('scene', 'hide-mesh-preview');
 };
