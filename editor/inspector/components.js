f927026fae81c970b6a1dc41ecc137ca4e916ac9
zhangxin <13622872688@163.com>
2021-03-25 11:47:13 +0800

    add polygon collider2d inspector

--- editor/inspector/components.js
+++ editor/inspector/components.js
@@ -4,4 +4,5 @@ const { join } = require('path');
 
 module.exports = {
     // 'cc.UITransform': join(__dirname, './components/class.js'),
+    'cc.PolygonCollider2D': join(__dirname, './components/polygon-collider.js'),
 };
---
+++ editor/inspector/components/polygon-collider.js
@@ -0,0 +1,102 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+const propUtils = require('../utils/prop');
+
+const excludeList = ['threshold'];
+exports.template = `
+<div class="polygon-collider-2d">
+    <div style="display: flex;">
+        <ui-prop 
+            style="flex: 1;margin-right: 5px"
+            type="dump"
+            key="threshold"
+        >
+        </ui-prop>
+        <div style="display: flex;align-content: center;align-items: center;">
+            <ui-button 
+                id="button"
+                class="blue tiny"
+            >
+                Regenerate Points
+            </ui-button>
+        </div>
+    </div>
+
+    <div id="customProps">
+    </div>
+</div>
+`;
+
+exports.methods = {
+    regenerate () {
+        Editor.Message.request('scene', 'regenerate-polygon-2d-points', this.dump.value.uuid.value);
+    },
+};
+exports.$ = {
+    button: '#button',
+    customProps: '#customProps',
+};
+const uiElements = {
+    buttton: {
+        ready () {
+            this.$.button.addEventListener('click', () => { this.regenerate(); });
+        },
+    },
+    baseProps: {
+        ready () {
+            this.$.baseProps = this.$this.querySelectorAll('ui-prop:not(.customProp)');
+            this.$.baseProps.forEach((element) => {
+                element.addEventListener('change-dump', () => {
+                    uiElements.baseProps.update.call(this);
+                });
+            });
+        },
+        update () {
+            if (!this.$.baseProps) {
+                uiElements.baseProps.ready.call(this);
+            }
+            this.$.baseProps.forEach((element) => {
+                const key = element.getAttribute('key');
+                const isShow = this.dump.value[key].visible;
+                if (isShow) {
+                    element.render(this.dump.value[key]);
+                }
+                element.style = isShow ? 'flex: 1;margin-right: 5px' : 'display: none;';
+            });
+        },
+    },
+    customProps: {
+        update () {
+            this.$.customProps.replaceChildren(...propUtils.getCustomPropElements(excludeList, this.dump, (element, prop) => {
+                element.className = 'customProp';
+                if (prop.dump.visible) {
+                    element.render(prop.dump);
+                }
+                element.style = prop.dump.visible ? '' : 'display: none;';
+            }));
+        },
+    },
+};
+
+exports.ready = function () {
+    for (const key in uiElements) {
+        const element = uiElements[key];
+        if (typeof element.ready === 'function') {
+            element.ready.call(this);
+        }
+    }
+};
+exports.update = function (dump) {
+    for (const key in dump.value) {
+        const info = dump.value[key];
+        if (dump.values) {
+            info.values = dump.values.map((value) => value[key].value);
+        }
+    }
+    this.dump = dump;
+    for (const key in uiElements) {
+        const element = uiElements[key];
+        if (typeof element.update === 'function') {
+            element.update.call(this);
+        }
+    }
+};
