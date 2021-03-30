89cd145f3aacf34b3c367a6c0a5791f82478f5b3
zhangxin <13622872688@163.com>
2021-03-30 14:49:00 +0800

    add button inspector

--- editor/inspector/components.js
+++ editor/inspector/components.js
@@ -3,5 +3,6 @@ const { join } = require('path');
 module.exports = {
     // 'cc.UITransform': join(__dirname, './components/class.js'),
     'cc.PageView': join(__dirname, './components/page-view.js'),
-    'cc.VideoPlayer': join(__dirname, './components/video-player.js')
+    'cc.VideoPlayer': join(__dirname, './components/video-player.js'),
+    'cc.Button': join(__dirname, './components/button.js'),
 };
