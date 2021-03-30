08ddada65b9cd3e120c11ae7e63c86785bb231bf
zhangxin <13622872688@163.com>
2021-03-30 14:33:22 +0800

    Update components.js

--- editor/inspector/components.js
+++ editor/inspector/components.js
@@ -3,6 +3,6 @@ const { join } = require('path');
 module.exports = {
     // 'cc.UITransform': join(__dirname, './components/class.js'),
     'cc.PageView': join(__dirname, './components/page-view.js'),
-    'cc.BatchedSkinningModel': join(__dirname, './components/batched-skinning-model.js'),
+    'cc.SkinnedMeshBatchRenderer': join(__dirname, './components/batched-skinning-model.js'),
     'cc.Button': join(__dirname, './components/button.js'),
 };
