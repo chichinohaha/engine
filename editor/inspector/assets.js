437363a9bf0a1b114b80abcbf8bacc43817b0d97
arsen2010 <arsen2010@126.com>
2021-04-12 11:11:32 +0800

    注册的组件按字母排序，以便剔除重复注册

--- editor/inspector/assets.js
+++ editor/inspector/assets.js
@@ -1,31 +1,30 @@
 const { join } = require('path');
 
 module.exports = {
+    'audio-clip': join(__dirname, './assets/audio-clip.js'),
+    'auto-atlas': join(__dirname, './assets/texture/auto-atlas.js'), // 复用
+    'dragonbones': join(__dirname, './assets/json.js'),  // 复用
+    'dragonbones-atlas': join(__dirname, './assets/json.js'), // 复用
     effect: join(__dirname, './assets/effect.js'),
+    'effect-header': join(__dirname, './assets/effect-header.js'),
+    'erp-texture-cube': join(__dirname, './assets/erp-texture-cube.js'),
     fbx: join(__dirname, './assets/fbx/index.js'),
     gltf: join(__dirname, './assets/fbx/index.js'), // 复用
     image: join(__dirname, './assets/image.js'),
+    json: join(__dirname, './assets/json.js'),
+    javascript: join(__dirname, './assets/javascript.js'),
+    'label-atlas': join(__dirname, './assets/label-atlas.js'),
+    material: join(__dirname, './assets/material.js'),
     prefab: join(__dirname, './assets/scene.js'), // 复用
+    particle: join(__dirname, './assets/particle.js'),
+    'physics-material': join(__dirname, './assets/physics-material.js'),
+    'render-texture': join(__dirname, './assets/render-texture.js'),
+    'render-pipeline': join(__dirname, './assets/render-pipeline.js'),
     scene: join(__dirname, './assets/scene.js'),
     'sprite-frame': join(__dirname, './assets/sprite-frame.js'),
-    material: join(__dirname, './assets/material.js'),
-    'label-atlas': join(__dirname, './assets/label-atlas.js'),
-    particle: join(__dirname, './assets/particle.js'),
-    json: join(__dirname, './assets/json.js'),
-    'dragonbones-atlas': join(__dirname, './assets/json.js'), // 复用
-    'dragonbones': join(__dirname, './assets/json.js'),  // 复用
+    text: join(__dirname, './assets/text.js'),
     texture: join(__dirname, './assets/texture/index.js'),
-    'auto-atlas': join(__dirname, './assets/texture/auto-atlas.js'), // 复用
-    material: join(__dirname, './assets/material.js'),
-    'erp-texture-cube': join(__dirname, './assets/erp-texture-cube.js'),
-    'effect-header': join(__dirname, './assets/effect-header.js'),
-    'audio-clip': join(__dirname, './assets/audio-clip.js'),
-    javascript: join(__dirname, './assets/javascript.js'),
+    'texture-cube': join(__dirname, './assets/texture-cube.js'),
     typescript: join(__dirname, './assets/typescript.js'),
     'video-clip': join(__dirname, './assets/video-clip.js'),
-    text: join(__dirname, './assets/text.js'),
-    'render-pipeline': join(__dirname, './assets/render-pipeline.js'),
-    'render-texture': join(__dirname, './assets/render-texture.js'),
-    'physics-material': join(__dirname, './assets/physics-material.js'),
-    'texture-cube': join(__dirname, './assets/texture-cube.js'),
 };
