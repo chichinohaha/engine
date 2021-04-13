247621fd1d46817c4ee6f85afdab418dfea3cf61
arsen2010 <arsen2010@126.com>
2021-04-13 14:41:39 +0800

    修改 cc.RichText 编辑

--- editor/inspector/components/rich-text.js
+++ editor/inspector/components/rich-text.js
@@ -1,104 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-const propUtils = require('../utils/prop');
+const { updatePropByDump, setHidden } = require('../utils/prop');
 
-const excludeList = [
-    'string', 'horizontalAlign', 'fontSize', 'useSystemFont',
-    'fontFamily', 'font', 'cacheMode', 'maxWidth', 'lineHeight',
-    'imageAtlas', 'handleTouchEvent',
-];
+const Elements = {
+    string: {
+        displayOrder: 0,
+    },
+    useSystemFont: {
+        displayOrder: 1,
+    },
+    fontFamily: {
+        displayOrder: 2,
+        update(element) {
+            setHidden(!this.dump.value.useSystemFont.value, element);
+        },
+    },
+    cacheMode: {
+        displayOrder: 3,
+        update(element) {
+            setHidden(!this.dump.value.useSystemFont.value, element);
+        },
+    },
+    font: {
+        displayOrder: 4,
+        update(element) {
+            setHidden(!!this.dump.value.useSystemFont.value, element);
+        },
+    },
+};
 
 exports.template = `
-<div class="rich-text-component">
-    <ui-prop type="dump" key="string"></ui-prop>
-    <ui-prop type="dump" key="horizontalAlign"></ui-prop>
-    <ui-prop type="dump" key="fontSize"></ui-prop>
-    <ui-prop type="dump" showflag="useSystemFont" key="fontFamily"></ui-prop>
-    <ui-prop type="dump" showflag="notUseSystemFont" key="font"></ui-prop>
-    <ui-prop type="dump" key="useSystemFont"></ui-prop>
-    <ui-prop type="dump" showflag="useSystemFont" key="cacheMode"></ui-prop>
-    <ui-prop type="dump" key="maxWidth"></ui-prop>
-    <ui-prop type="dump" key="lineHeight"></ui-prop>
-    <ui-prop type="dump" key="imageAtlas"></ui-prop>
-    <ui-prop type="dump" key="handleTouchEvent"></ui-prop>
-    <!-- Render other data that is not taken over -->
-    <div id="customProps"></div>
+<div class="component-container">
 </div>
 `;
 
 exports.$ = {
-    button: '#button',
-    customProps: '#customProps',
-    useSystemFont: 'ui-prop[key="useSystemFont"]',
-};
-const uiElements = {
-    useSystemFont: {
-        ready () {
-            this.$.useSystemFont.addEventListener('change-dump', () => {
-                uiElements.baseProps.update.call(this);
-            });
-        },
-    },
-    baseProps: {
-        ready () {
-            this.$.baseProps = this.$this.querySelectorAll('ui-prop:not(.customProp)');
-            this.$.showflags = [];
-        },
-        update () {
-            if (!this.$.baseProps) {
-                uiElements.baseProps.ready.call(this);
-            }
-            this.$.baseProps.forEach((element) => {
-                const key = element.getAttribute('key');
-                let isShow = this.dump.value[key].visible;
-          
-                if (element.hasAttribute('showflag')) {
-                    const showflag = element.getAttribute('showflag');
-                    if (showflag === 'useSystemFont') {
-                        isShow = isShow && this.dump.value.useSystemFont.value;
-                    } else if (showflag === 'notUseSystemFont') {
-                        isShow = isShow && !this.dump.value.useSystemFont.value;
-                    }
-                }
-                if (isShow) {
-                    element.render(this.dump.value[key]);
-                }
-                element.style = isShow ? '' : 'display: none;';
-            });
-        },
-    },
-    customProps: {
-        update () {
-            this.$.customProps.replaceChildren(...propUtils.getCustomPropElements(excludeList, this.dump, (element, prop) => {
-                element.className = 'customProp';
-                if (prop.dump.visible) {
-                    element.render(prop.dump);
-                }
-                element.style = prop.dump.visible ? '' : 'display: none;';
-            }));
-        },
-    },
+    componentContainer: '.component-container',
 };
 
-exports.ready = function () {
-    for (const key in uiElements) {
-        const element = uiElements[key];
-        if (typeof element.ready === 'function') {
-            element.ready.call(this);
-        }
-    }
-};
 exports.update = function (dump) {
-    for (const key in dump.value) {
-        const info = dump.value[key];
-        if (dump.values) {
-            info.values = dump.values.map((value) => value[key].value);
-        }
-    }
     this.dump = dump;
-    for (const key in uiElements) {
-        const element = uiElements[key];
-        if (typeof element.update === 'function') {
-            element.update.call(this);
-        }
-    }
+    updatePropByDump(this, dump, Elements);
 };
